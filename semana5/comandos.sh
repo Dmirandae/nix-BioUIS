d44457a133ed8cb90d16c0309a421b48e0ba27ba
Rafael Miranda-Esquivel <dmirandae@gmail.com>
2025-02-13 09:49:30 -0500

    update week5 to git

---
+++ semana5/comandos.sh
@@ -0,0 +1,70 @@
+## para imprimir desden hasta el final
+
+cat historia | awk '{for(i=2;i<=NF;++i)printf $i" " ; print ""}'
+
+
+# parte 1
+
+## https://product.hubspot.com/blog/git-and-github-tutorial-for-beginners
+
+
+
+# parte 2
+
+## https://graphite.dev/guides/checkout-file-from-prev-commit
+
+git log crearManual.sh 
+git log | less 
+git log | grep ^ 
+git log | grep data 
+git log | less 
+git log | grep data 
+git log | grep "data on" 
+git log | grep -A 4 "data on" 
+git log | less 
+git log | grep -B 4 "data on" 
+git log crearManual.sh 
+git log blepd.pdf 
+git log blepd.pdf 
+## revisar con git 
+git log blepd.pdf 
+## git log blepd.pdf 
+## git log archivo para ver los commits 
+git log blepd.pdf 
+git log blepd.pdf | grep just 
+git log blepd.pdf | grep B -3 just 
+git log blepd.pdf | grep B 3 just 
+git log blepd.pdf | grep -B 3 just 
+git log blepd.pdf | grep -B 4 just 
+git log blepd.pdf | grep -B 4 just | grep com} 
+git log blepd.pdf | grep -B 4 just | grep com 
+git log blepd.pdf | grep -B 4 just | grep comm 
+git log blepd.pdf | grep -B 4 just | grep comm | awk '{print $2}' 
+echo {git log blepd.pdf | grep -B 4 just | grep comm | awk '{print $2}'} > t1 
+echo ${git log blepd.pdf | grep -B 4 just | grep comm | awk '{print $2}'} > t1 
+echo ${git log blepd.pdf | grep -B 4 just | grep comm | awk '{print $2}'} > t1 
+echo ${git log blepd.pdf | grep -B 4 just | grep comm | awk '{print $2}'} 
+git log blepd.pdf | grep -B 4 just | grep comm | awk '{print $2}'} 
+git log blepd.pdf | grep -B 4 just | grep comm | awk '{print $2}' 
+git log blepd.pdf | grep -B 4 just | grep comm | awk '{print $2}' > t1 
+cat t1 
+evince blepd.pdf & 
+cat t1 
+${cat t1} 
+echo ${cat t1} 
+echo $[cat t1] 
+cat t1 
+geany t1 
+git checkout b8165d6d55b5b1fcb81d1978a41c172bd525d52a blepd.pdf 
+ls -lsh blepd.pdf 
+git checkout b8165d6d55b5b1fcb81d1978a41c172bd525d52a blepd.pdf 
+sh crearManual.sh 
+ls -lsh blepd.pdf 
+ls 
+cat crearManual.sh 
+ls 
+ls -lsh blepd.pdf 
+echo "t1" > blepd.pdf 
+ls -lsh blepd.pdf 
+git checkout b8165d6d55b5b1fcb81d1978a41c172bd525d52a blepd.pdf 
+ls -lsh blepd.pdf 
